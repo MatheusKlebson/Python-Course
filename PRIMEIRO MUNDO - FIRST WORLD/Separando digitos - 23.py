b00a008019076f25666fce200aa6d40662fcc5f9
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-16 11:45:16 -0300

    Códigos Python

---
+++ PRIMEIRO MUNDO - FIRST WORLD/Separando digitos - 23.py
@@ -0,0 +1,14 @@
+#Exercício Python 23: Faça um programa que leia um número de 0 a 9999 
+#mostre na tela cada um dos dígitos separados.
+from time import sleep
+num = int(input("Digite um número: "))
+print("Analisando...")
+sleep(2)
+u = num // 1 % 10 
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f"Unidade = {u}")
+print(f"Dezena = {d}")
+print(f"Centena = {c}")
+print(f"Milhar = {m}")
