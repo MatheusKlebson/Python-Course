506ed662346ecca102aec20e7f09ccc18fc73477
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-16 11:18:00 -0300

    Códigos Python

---
+++ PRIMEIRO MUNDO - FIRST WORLD/Respondendo ao usuário - 2.py
@@ -0,0 +1,4 @@
+#Exercício Python 002: Faça um programa que leia o nome de uma pessoa
+#mostre uma mensagem de boas-vindas.
+nome = str(input("Nome de alguém: "))
+print("Seja bem vindo!! É um prazer conhecer voce",nome)
