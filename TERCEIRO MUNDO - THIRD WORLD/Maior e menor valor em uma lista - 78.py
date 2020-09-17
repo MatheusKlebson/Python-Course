bcf8250d7e89f26baedd5c1d33c18aa02bf15ed4
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-17 10:02:36 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Maior e menor valor em uma lista - 78.py
+++ TERCEIRO MUNDO - THIRD WORLD/Maior e menor valor em uma lista - 78.py
@@ -1,25 +1,30 @@
 #Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. 
-#No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
-lista = list()
-maior = 0
-menor = 0
-for cont in range(0,5):
-    lista.append(int(input(f"Número na {cont+1}º Posição: ")))
-    if cont == 0:
-        maior = lista[cont]
-        menor = lista[cont]
-    else:
-        if maior < lista[cont]:
-            maior = lista[cont]
-        if menor > lista[cont]:
-            menor = lista[cont]
-print(f"A lista gerada: {lista}")
+#No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista
+valores = list()
+print("="*60)
+print("{:^60}".format("CRIE UMA LISTA DE NÚMEROS"))
+print("="*60)
+totnum = int(input("Digite a quantidade de números que estaŕa na lista: "))
 print("="*30)
-print(f"Maior da lista: {maior}")
-print(f"Menor da lista: {menor}")
+print("{:^30}".format("FIQUE A VONTADE"))
 print("="*30)
-for p, v in enumerate(lista):
-    if maior == v:
-        print(f"Posição do maior: {p+1}º")
-    if menor == v:
-        print(f"Posição do menor: {p+1}")
+for cont in range(0,totnum):
+  valores.append(int(input(f"Valor na posição {cont+1}: ")))
+print("="*45)
+print("{:^45}".format(f"LISTA DE {totnum} NÚMEROS FOI FORMADA : {valores}"))
+print("="*45)
+maior = max(valores)
+menor = min(valores)
+print(f"Maior valor: {maior}")
+print(f"Localização do maior valor: ",end="")
+for p, v in enumerate(valores):
+  if v == maior:
+    print(f"[{p+1}]",end="")
+print()
+print("="*45)
+print(f"Menor valor: {menor}")
+print(f"Localização do menor valor: ",end="")
+for p, v in enumerate(valores):
+  if v == menor:
+    print(f"[{p+1}]",end="")
+
