220d67fcae4e0862c3c6750273d7deb8bf3301d3
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-19 11:25:11 -0300

    Códigos Python

---
+++ TERCEIRO MUNDO - THIRD WORLD/Dividindo os valores em uma lista - 82.py
@@ -0,0 +1,23 @@
+#Exercício Python 082: Crie um programa que vai ler vários números e colocar em uma lista. 
+# Depois disso:
+# crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados,
+# respectivamente. 
+# Ao final, mostre o conteúdo das três listas geradas.
+lista = []
+impares = []
+pares = []
+while True:
+    num = int(input("Digite um número: "))
+    lista.append(num)
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+    opção = " "
+    while not opção in "SN":
+        opção = str(input("Deseja continuar[S/N]? ")).strip().upper()[0]
+    if opção == "N":
+        break
+print(f"Lista: {lista}")
+print(f"Lista pares: {pares}")
+print(f"Lista impares: {impares}")
