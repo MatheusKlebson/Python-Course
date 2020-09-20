ff0f9730841f769b2df5f8e60cce15658e167c3d
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-20 16:13:32 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Organizando uma lista sem o sort() - 80.py
+++ TERCEIRO MUNDO - THIRD WORLD/Organizando uma lista sem o sort() - 80.py
@@ -1,5 +1,7 @@
 #Exercício Python 080: Crie um programa onde o usuário possa digitar cinco valores numéricos
 #cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+#Exercício Python 080: Crie um programa onde o usuário possa digitar cinco valores numéricos
+#cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 lista = []
 for cont in range(0,5):#For que contará os valores até 5
   num = int(input(f"Digite {cont}º valor: ")) #Variavel que guardará os valores digitados
