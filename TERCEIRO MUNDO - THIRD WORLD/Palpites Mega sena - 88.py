ed69c9ae0dfde8fb16b80a3b6374e2fb4f68d780
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-22 09:21:34 -0300

    Códgios Python

--- TERCEIRO MUNDO - THIRD WORLD/Palpites Mega sena - 88.py
+++ TERCEIRO MUNDO - THIRD WORLD/Palpites Mega sena - 88.py
@@ -2,26 +2,18 @@
 # criar palpites.O programa vai perguntar quantos jogos serão gerados 
 # vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 from random import randint
-from time import sleep
-sorteados = []
-jogos = []
+listas = []
 print("="*30)
 print("{:^30}".format("PALPITES DA MEGA SENA"))
 print("="*30)
-totjogos = int(input("QUANTOS JOGOS DESEJA SORTEAR: "))
-tot = 0
-while tot < totjogos:
+totjogos = int(input("Quantos jogos deseja sortear? "))
+totcadastros = 0
+while totcadastros < totjogos:
     cont = 0
     while True:
-        numeros = randint(1,60)
-        if numeros not in sorteados: 
-            sorteados.append(numeros)
+        numero = randint(1,60)
+        if numero not in listas:
+            listas.append(numero)
             cont += 1
         if cont == 6:
             break
-    tot += 1
-    jogos.append(sorteados[:])
-    sorteados.clear()
-for indice,lista in enumerate(jogos):
-    print(f"JOGO {indice}: {sorted(lista)}")
-    sleep(1)
