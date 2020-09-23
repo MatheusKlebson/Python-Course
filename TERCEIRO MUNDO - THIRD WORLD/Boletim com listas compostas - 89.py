296b007e148add905c7d7575702531c4cf9b62ff
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-23 11:13:56 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Boletim com listas compostas - 89.py
+++ TERCEIRO MUNDO - THIRD WORLD/Boletim com listas compostas - 89.py
@@ -2,12 +2,13 @@
 #   guarde tudo em uma lista composta. 
 #   No final, mostre um boletim contendo a média de cada um 
 #   permita que o usuário possa mostrar as notas de cada aluno individualmente.
-lista = [[],[],[]]
+lista = []
 while True:
-    lista[0].append(str(input("Digite o nome do aluno: ")).strip().title())
-    lista[1].append(float(input("Digite a primeira nota: ")))
-    lista[1].append(float(input("Digute a segunda nota: ")))
-    lista[2].append((lista[1][0] + lista[1][1]/2))
+    nome = str(input("Digite o nome do aluno: ")).strip().title()
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    media = (nota1 + nota2)/2
+    lista.append([nome,[nota1,nota2],media])
     opção = " "
     while not opção in "SN":
         opção = str(input("Deseja continuar[S/N]? ")).strip().upper()[0]
