3b99f5d2a499a417830751605b3fe30b8f49c06d
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-23 10:53:37 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Palpites Mega sena - 88.py
+++ TERCEIRO MUNDO - THIRD WORLD/Palpites Mega sena - 88.py
@@ -1,11 +1,20 @@
 # Exercício Python 088: Faça um programa que ajude um jogador da MEGA SENA  
 # criar palpites.O programa vai perguntar quantos jogos serão gerados 
 # vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
-
+lista = []
 print("="*30)
 print("{:^30}".format("PALPITES DA MEGA SENA"))
 print("="*30)
 totjogos = int(input("Quantos jogos deseja sortear? "))
 tot = 0
 while tot < totjogos:
-    
+    cont = 0
+    while True:
+        numero = randint(1,60)
+        if not numero in lista:
+            lista.append(numero)
+            cont += 1
+        if cont == 6:
+            break
+    tot += 1
+print(lista)
