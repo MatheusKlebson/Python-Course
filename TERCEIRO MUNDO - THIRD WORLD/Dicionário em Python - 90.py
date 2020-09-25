4d448580db1c931af22befd62250790dcff655d4
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-25 12:38:19 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Dicionário em Python - 90.py
+++ TERCEIRO MUNDO - THIRD WORLD/Dicionário em Python - 90.py
@@ -10,6 +10,21 @@ elif aluno["Média"] >= 5 and aluno["Média"] < 7:
 elif aluno["Média"] >= 0 and aluno["Média"] < 5:
     aluno["Situação"] = "REPROVADO"
 print("-="*30)
-
 for v, k in aluno.items():
     print(f" - {v} = {k}")
+
+#Outra forma de adicionar no Diciónario
+nome = str(input("Digite o nome do aluno: ")).strip().title()
+media = float(input(f"Média do {nome}: "))
+if media >= 7 and media <= 10:
+    situacao = "APROVADO"
+elif media > 4.9 and media < 7:
+    situacao = "RECUPERAÇÃO"
+elif media >= 0 and media < 5:
+    situacao = "REPROVADO"
+else:
+    print("O aluno possui uma média inválida...")
+aluno = {"Nome":nome,"Media":media,"Situação":situacao}
+print("="*50)
+for k,v in aluno.items():
+    print(f"- {k} é igual a {v}")
