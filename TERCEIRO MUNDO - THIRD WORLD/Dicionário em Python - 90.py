f82c906805ea22a67b5f5795e43bf90388ed0c9a
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-25 12:35:07 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Dicionário em Python - 90.py
+++ TERCEIRO MUNDO - THIRD WORLD/Dicionário em Python - 90.py
@@ -3,3 +3,13 @@
 aluno = {}
 aluno["Nome"] = str(input("Nome do aluno: ").strip().title())
 aluno["Média"] = float(input(f"Média do {aluno['Nome']}: "))
+if aluno["Média"] >= 7 and aluno["Média"] <= 10:
+    aluno["Situação"] = "APROVADO"
+elif aluno["Média"] >= 5 and aluno["Média"] < 7:
+    aluno["Situação"] = "RECUPERAÇÃO"
+elif aluno["Média"] >= 0 and aluno["Média"] < 5:
+    aluno["Situação"] = "REPROVADO"
+print("-="*30)
+
+for v, k in aluno.items():
+    print(f" - {v} = {k}")
