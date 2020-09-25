26b2f91dd934398c474aa8acd39ea6e65c3a4d8a
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-25 12:27:39 -0300

    Resolução do professor

--- TERCEIRO MUNDO - THIRD WORLD/Dicionário em Python - 90.py
+++ TERCEIRO MUNDO - THIRD WORLD/Dicionário em Python - 90.py
@@ -1,16 +1,5 @@
 #Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. 
 # No final, mostre o conteúdo da estrutura na tela.
-nome = str(input("Digite o nome do aluno: ")).strip().title()
-media = float(input(f"Média do {nome}: "))
-if media >= 7 and media <= 10:
-    situacao = "APROVADO"
-elif media > 4.9 and media < 7:
-    situacao = "RECUPERAÇÃO"
-elif media >= 0 and media < 5:
-    situacao = "REPROVADO"
-else:
-    print("O aluno possui uma média inválida...")
-aluno = {"Nome":nome,"Media":media,"Situação":situacao}
-print("="*50)
-for k,v in aluno.items():
-    print(f"- {k} é igual a {v}")
+aluno = {}
+aluno["Nome"] = str(input("Nome do aluno: ").strip().title())
+aluno["Média"] = float(input(f"Média do {aluno['Nome']}: "))
