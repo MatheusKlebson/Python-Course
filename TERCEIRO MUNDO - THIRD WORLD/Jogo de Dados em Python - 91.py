b47b5847b0e52bbe2e9de50bd69f28df67587e75
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-26 09:26:50 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Jogo de Dados em Python - 91.py
+++ TERCEIRO MUNDO - THIRD WORLD/Jogo de Dados em Python - 91.py
@@ -3,10 +3,12 @@
 #No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 from random import randint
 from time import sleep
+from operator import itemgetter
 jogadores = {}
 for cont in range(1,5):
     dado = randint(1,6)
     print(f"O jogador {cont} sorteou o número {dado} no dado...") 
-    jogadores["Jogador {cont}"] = dado
+    jogadores[f"Jogador {cont}"] = dado
     sleep(1)
+ranking = sorted(jogadores.items(),key=itemgetter(1),reverse=True)
 print(jogadores)
