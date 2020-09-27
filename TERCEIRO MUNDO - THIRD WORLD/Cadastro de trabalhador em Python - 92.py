f175bbed092b9b22db9b25520dd9d7e875b6fb14
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-27 17:53:11 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Cadastro de trabalhador em Python - 92.py
+++ TERCEIRO MUNDO - THIRD WORLD/Cadastro de trabalhador em Python - 92.py
@@ -2,8 +2,11 @@
 # carteira de trabalho e cadastre-o (com idade) em um dicionário. 
 # Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. 
 # Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+from datetime import date
 dados = {}
 dados["Nome"] = str(input("Nome do cidadão: ")).strip().title()
-dados["AnoNasc"] = int(input("Ano de Nascimento: "))
+AnoNasc = int(input("Ano de Nascimento: "))
+AnoAtual = date.today().year
+dados["Idade"] = AnoNasc - AnoAtual
 dados["Carteira"] = int(input("Número da carteira de trabalho (0 caso não tenha): "))
 print(dados)
