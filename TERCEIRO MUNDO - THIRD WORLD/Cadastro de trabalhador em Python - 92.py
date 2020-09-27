78fd37e41550c06054e4f221d5863a256500d0a8
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-27 18:49:12 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Cadastro de trabalhador em Python - 92.py
+++ TERCEIRO MUNDO - THIRD WORLD/Cadastro de trabalhador em Python - 92.py
@@ -9,19 +9,22 @@ AnoNasc = int(input("Ano de Nascimento: "))
 AnoAtual = date.today().year
 dados["Idade"] = AnoAtual - AnoNasc 
 dados["Carteira"] = int(input("Número da carteira de trabalho (0 caso não tenha): "))
+
 if dados["Carteira"] != 0:
     dados["Ano de contratação"] = int(input("Ano de contratação: "))
     dados["Salário"] = float(input("Salário do funcionário:R$ "))
     AnoAposentadoria = dados["Ano de contratação"] + 35
     IdadeAposentadoria = AnoAposentadoria - AnoNasc
     dados["Aposentadoria"] = IdadeAposentadoria
-    print("="*50)
+print("="*50)
+if dados["Carteira"] == 0:
+    print(f'''    - NOME: {dados["Nome"]}
+    - IDADE: {dados["Idade"]}
+    - CTPS: Não possui, o cidadão está desempregado''')
+if dados["Carteira"] != 0:
     print(f'''    - NOME: {dados["Nome"]}
     - IDADE: {dados["Idade"]}
     - CTPS: {dados["Carteira"]}
     - Ano de contratação: {dados["Ano de contratação"]}
     - Salário: R${dados["Salário"]}
     - Idade da aposentadoria: {dados["Aposentadoria"]}''')
-    
-'''if dados["Carteira"] == 0:
-    print(f"CTPS: Não possui, o cidadão está desempregado")'''
