795648a5329bbd4f470f251d57544eb663c20f4c
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-28 20:10:21 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Cadastro de jogador de futebol - 93.py
+++ TERCEIRO MUNDO - THIRD WORLD/Cadastro de jogador de futebol - 93.py
@@ -3,7 +3,9 @@
 # Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, 
 # incluindo o total de gols feitos durante o campeonato.
 jogador = {}
+gols = []
 jogador["Nome"] = str(input("Nome do jogador: ").strip().title()
 partidas = int(input("Total de partidas jogadas: "))
-for jogos in range(0,partidas)
-jogador[""] = 
+for jogos in range(0,partidas):
+    gols.append(int(input(f"Total de gols feitos na {jogos}º Partida: ")))
+print(gols)
