d394eb9d05d8400ea8b37ff2ca2e7978468d2635
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-29 11:32:22 -0300

    Códigos Python

---
+++ TERCEIRO MUNDO - THIRD WORLD/Unindo dicionários e listas - 94.py
@@ -0,0 +1,17 @@
+#Exercício Python 094: Crie um programa que leia nome, sexo e idade de várias pessoas, 
+# guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. 
+# No final, mostre: 
+# A) Quantas pessoas foram cadastradas 
+# B) A média de idade 
+# C) Uma lista com as mulheres 
+# D) Uma lista de pessoas com idade acima da média
+dados = []
+pessoa = {}
+pessoa["Nome"] = str(input("Nome da Pessoa: ")).strip().title()
+pessoa["Idade"] = int(input("Idade: "))
+sexo["Sexo"] = str(input("Sexo: [M/F]")).strip().upper().[0]
+option = " "
+while not option in "SN":
+    option = str(input("Deseja continuar[S/N]? ")).strip().upper()[0]
+    if option == "N":
+        break
