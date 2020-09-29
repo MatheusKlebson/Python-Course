2039f1adf9cc4aa395afa027f0a7ef942ad9c907
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-09-29 11:39:18 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Unindo dicionários e listas - 94.py
+++ TERCEIRO MUNDO - THIRD WORLD/Unindo dicionários e listas - 94.py
@@ -10,10 +10,11 @@ pessoa = {}
 while True:
     pessoa["Nome"] = str(input("Nome da Pessoa: ")).strip().title()
     pessoa["Idade"] = int(input("Idade: "))
-    sexo["Sexo"] = str(input("Sexo: [M/F]")).strip().upper()[0]
-
+    pessoa["Sexo"] = str(input("Sexo: [M/F]")).strip().upper()[0]
+    dados.append(pessoa.copy())
     option = " "
     while not option in "SN":
         option = str(input("Deseja continuar[S/N]? ")).strip().upper()[0]
-        if option == "N":
-            break
+    if option == "N":
+        break
+print(dados)
