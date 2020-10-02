e8dfd4319b3ec448a85ff8f241bf2800bee4c1cb
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-02 07:11:21 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Aprimorando diciónarios - 95.py
+++ TERCEIRO MUNDO - THIRD WORLD/Aprimorando diciónarios - 95.py
@@ -1,36 +1,8 @@
 # Exercício Python 095: Aprimore o desafio 93 para que ele funcione com vários jogadores, x 
 # incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
-jogadores = []
-while True:
-    jogador = {}
-    gol = []
-    totgol = 0
-    jogador["Nome"] = str(input("Nome do jogador: ").strip().title())
-    partidas = int(input(f"Total de partidas que {jogador['Nome']} jogou: "))
-    for jogos in range(1,partidas + 1):
-        golPartida = int(input(f"Total de gols feitos na {jogos}º Partida: "))
-        gol.append(golPartida)
-        totgol += golPartida
-    jogador["Gols"] = gol
-    jogador["Total de Gols"] = totgol
-    jogadores.append(jogador.copy())
+jogador = dict()
+partidas = list()
+jogador["Nome"] = str(input("Nome do jogador: ")).strip().title()
 
-    option = " "
-    while not option in "SN":
-        option = str(input("Deseja continuar[S/N]? ")).strip().upper()[0]
-    if option == "N":
-        break
-print(jogadores)
-'''Discionario jogador, Estrutura: {'Nome': 'Matheus', 'Gols': [0, 4, 8, 6, 3], 'Total de Gols': 21}'''
-'''[{'Nome': 'Matheus', 'Gols': [0, 3], 'Total de Gols': 3}, {'Nome': 'Micaias', 'Gols': [2, 0, 1], 'Total de Gols': 3}]'''
-print("="*65)
-print("{:^65}".format("DADOS DOS JOGADORES"))
-print("="*65)
-#print(f"{'NUM':>5}{'NOME':>15}{'GOLS':>20}{'TOTAL DE GOLS':>23}")
-print("NUM",end="")
-for i in jogador.items():
-    print(f"{i:<15}",end="")
-print()
-print('-'*65)
-#for indice,player in enumerate(jogadores):
-    #print(f"{indice:>5}{player['Nome']:>15}")
+'''ASSISTI A AULA, SE PRECISO COPIA O CÓDIGO DO GUANABARA E ESTUDA COM CALMA, 
+É O ULTIMO EXERCICIO FAZ SENTIDO NÃO TA CONSEGUINDO RESOLVER'''
