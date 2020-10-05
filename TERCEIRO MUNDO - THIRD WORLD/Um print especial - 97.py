c49bea68ad2927e0056904b5609da5bb57b20f7a
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-05 09:46:35 -0300

    Códigos Python

---
+++ TERCEIRO MUNDO - THIRD WORLD/Um print especial - 97.py
@@ -0,0 +1,16 @@
+# Exercício Python 097: Faça um programa que tenha uma função chamada escreva(), 
+# que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.                                  
+# Ex:                                                      
+# escreva(‘Olá, Mundo!’) Saída:                                                                                                                          
+# ~~~~~~~~~                                                                                                                                                            
+# Olá, Mundo!                                                                                                                                                          
+# ~~~~~~~~~  
+def escreva(msg):
+    tam = len(msg) + 5
+    print("="*tam)
+    print(f"{msg}")
+    print('='*tam)
+
+escreva("MATHEUS É LINDO")
+escreva("HELLO WORLD")
+escreva("PROGRAMMING IS LIFE")
