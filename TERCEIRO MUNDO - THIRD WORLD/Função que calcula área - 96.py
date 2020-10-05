0807dce6148c85384837c60fe86afbbb21a798b2
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-05 09:23:22 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Função que calcula área - 96.py
+++ TERCEIRO MUNDO - THIRD WORLD/Função que calcula área - 96.py
@@ -2,10 +2,11 @@
 # que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
 def mostraLinha(txt):
     print("=" * 50)
-    print(txt)
+    print("{:^50}".format(txt))
     print("=" * 50)
 def area(largura,comprimento):
     area = (largura * comprimento)/2
     print(f"A dimensão da area {largura}X{comprimento} é de {area}m2")
-mostraLinha("CUMPRIMENTO")
-
+mostraLinha("CUMPRIMENTO DA ÁREA")
+l = float(input("Largura: "))
+c = float(input("Cumprimento: "))
