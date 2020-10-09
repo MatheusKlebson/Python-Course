f13e23f9df812d30c277d6548570dd07df522262
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-09 11:53:48 -0300

    Códigos Python

---
+++ TERCEIRO MUNDO - THIRD WORLD/Função que descobre o maior valor - 99.py
@@ -0,0 +1,11 @@
+# Exercício Python 099: Faça um programa que tenha uma função chamada maior(), 
+# que receba vários parâmetros com valores inteiros. 
+# Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+def maior(* num):
+    print(num)
+
+
+maior(6,8,3,1,6,7,10)
+maior(2,4,5,3,1)
+maior(5,3,1)
+maior(0)
