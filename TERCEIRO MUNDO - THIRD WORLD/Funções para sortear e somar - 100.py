79751a32aea7ea33b1b5a94c5d5776caca75fd36
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-10 23:38:29 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Funções para sortear e somar - 100.py
+++ TERCEIRO MUNDO - THIRD WORLD/Funções para sortear e somar - 100.py
@@ -1,11 +1,13 @@
 # Exercício Python 100: Faça um programa que tenha uma lista chamada números 
 # duas funções chamadas sorteia() e somaPar(). A primeira função vai sortear 5 números 
 # vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
+from random import randint
 numeros = []
 def sorteia():
-
-
-def somaPar():
-
-
+    for cont in range(0,5):
+        num = randint(1,10)
+        numeros.append(num)
     
+#def somaPar():
+sorteia()
+print(numeros)
