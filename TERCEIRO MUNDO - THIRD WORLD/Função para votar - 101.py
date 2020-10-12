5ed2a37d01e257dd8683b9d303675fa3dde3e3c5
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-12 20:30:58 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Função para votar - 101.py
+++ TERCEIRO MUNDO - THIRD WORLD/Função para votar - 101.py
@@ -7,8 +7,11 @@ def voto(anoNasc):
     print(f"Com {idade} anos - ",end="")
     if idade >= 18 and idade < 70:
         print("VOTO: OBRIGATÓRIO")
-    elif idade >= 16 and idade < 18:
+    elif idade >= 16 and idade < 18 or idade >= 70:
         print("VOTO: OPCIONAL")
+    else:
+        print("VOTO: NEGADO")
+
 
 print("="*50)
 ano = int(input("Em que ano você nasceu: "))
