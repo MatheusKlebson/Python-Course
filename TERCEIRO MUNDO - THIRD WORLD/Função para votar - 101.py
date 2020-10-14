7beb8e766c757ce93b8c72cdc5117f591a338032
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-13 21:20:39 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Função para votar - 101.py
+++ TERCEIRO MUNDO - THIRD WORLD/Função para votar - 101.py
@@ -3,16 +3,8 @@
 # retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 def voto(anoNasc):
     from datetime import date
-    idade = date.today().year - anoNasc
-    print(f"Com {idade} anos - ",end="")
-    if idade >= 18 and idade < 70:
-        return "VOTO: OBRIGATÓRIO"
-    elif idade >= 16 and idade < 18 or idade >= 70:
-        return "VOTO: OPCIONAL"
-    else:
-        return "VOTO: NEGADO"
+    anoAtual = date.today().year
 
 
-print("="*50)
-ano = int(input("Em que ano você nasceu: "))
-voto(ano)
+nascimento = int(input("Ano que você nasceu: "))
+voto(nascimento)
