434b3b24e84af968971f90469bf31094963caffc
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-13 21:08:23 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Função para votar - 101.py
+++ TERCEIRO MUNDO - THIRD WORLD/Função para votar - 101.py
@@ -1,8 +1,8 @@
 # Exercício Python 101: Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro 
 # ano de nascimento de uma pessoa, 
 # retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
-from datetime import date
 def voto(anoNasc):
+    from datetime import date
     idade = date.today().year - anoNasc
     print(f"Com {idade} anos - ",end="")
     if idade >= 18 and idade < 70:
