48415577f6778b71af275836096a0ea78a0fc80a
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-15 11:43:46 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Função para fatorial - 102.py
+++ TERCEIRO MUNDO - THIRD WORLD/Função para fatorial - 102.py
@@ -2,6 +2,7 @@
 #  o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional)
 #  indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 def fatorial(calcular,show):
+    print("="*50)
     if show == True:
         f = 1
         for c in range(calcular,0,-1):
@@ -17,5 +18,6 @@ def fatorial(calcular,show):
         for c in range(calcular,0,-1):
             f *= c
         return f
+print("="*50)
 num = int(input("Digite um número para o fatorial: "))
 print(fatorial(num,show=True))
