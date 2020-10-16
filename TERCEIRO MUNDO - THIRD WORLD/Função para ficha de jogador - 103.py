107fd07cabdb20cb84480e53e9440ae62d895c78
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-16 17:49:49 -0300

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Função para ficha de jogador - 103.py
+++ TERCEIRO MUNDO - THIRD WORLD/Função para ficha de jogador - 103.py
@@ -3,8 +3,9 @@
 # quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, 
 # mesmo que algum dado não tenha sido informado corretamente.
 def ficha(jogador="<desconhecido>",gols=0):
-
+    print(f"O jogador {jogador} fez {gols} no campeonato")
 
 print("="*50)
 nome = str(input("Nome do jogador: ").strip().title())
 gol = int(input(f"Total de gols que {nome} fez: "))
+ficha(nome,gol)
