414e3aac60e1b790c432cabeced6602ce32963db
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-20 10:12:07 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Validando dados usando função - 104.py
+++ TERCEIRO MUNDO - THIRD WORLD/Validando dados usando função - 104.py
@@ -3,11 +3,16 @@
 # só que fazendo a validação para aceitar apenas um valor numérico. 
 # Ex: n = leiaInt(‘Digite um n: ‘)
 def readInt(message):
-    number = str(input(message))
-    if number.isnumeric():
-        value = number 
-    else:
-        print("ERROR!! YOU NOT WRITED A NUMBER")
+    okay = False
+    while True:
+        number = str(input(message))
+        if number.isnumeric():
+            value = number
+            okay = True 
+        else:
+            print("ERROR!! YOU NOT WRITED A NUMBER")
+        if okay == True:
+            break
     return value
 number = readInt("Write a number: ")
 print(f"You writed a number {number}, Thanks!!")
