c6ea5a1d2d53194a95858af8a10a40a5a47ac6dd
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-20 10:48:22 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Analisando e gerando Dicionários - 105.py
+++ TERCEIRO MUNDO - THIRD WORLD/Analisando e gerando Dicionários - 105.py
@@ -5,13 +5,31 @@
 – A menor nota                                                                                                                                                              
 – A média da turma                                                                                                                                                      
 – A situação (opcional)'''
-def notes(*n):
+def notes(*n,situation=False):
+    '''
+    FUNCTION FOR CALCULATING MANY NUMBERS:
+    def(*n,situation=False) 
+    RECEIVES PARAMETERS
+    (ACCEPT MANY NUMBERS) *n = numbers 
+    (OPTIONAL PARAMETER) situation = medium situation
+    RETURN DICTIONARY
+    '''
     notebook = dict()
     notebook["NotesTotal"] = len(n)
     notebook["FisrtNote"] = max(n)
     notebook["LastNote"] = min(n)
+    notebook["Medium"] = sum(n)/len(n)
+    if situation == True:
+        if notebook["Medium"] >= 7 and notebook["Medium"] <= 10:
+            notebook["Situation"] = "Very Good"
+        elif notebook["Medium"] >= 5 and notebook["Medium"] < 7:
+            notebook["Situation"] = "Reasonable"
+        elif notebook["Medium"] >= 0 and notebook["Medium"] < 5:
+            notebook["Situation"] = "Very bad"
+        else:
+             notebook["Situation"] = "Medium invalid"
     return notebook
 
 
-response = notes(10,5,3,4)
+response = notes(10,8,7,situation=True)
 print(response)
