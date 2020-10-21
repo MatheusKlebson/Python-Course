450478c5d3467855e43a56675ffae46461b57677
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-21 17:03:01 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Interactive helping system in Python - 106.py
+++ TERCEIRO MUNDO - THIRD WORLD/Interactive helping system in Python - 106.py
@@ -1,7 +1,18 @@
 # Exercício Python 106: Faça um mini-sistema que utilize o Interactive Help do Python. 
 # O usuário vai digitar o comando e o manual vai aparecer. 
 # Quando o usuário digitar a palavra ‘FIM’, o programa se encerrará. Importante: use cores.
+def ajuda(com):
+    help(com)
+
+
+def titulo(txt):
+    tam = len(txt) + 4
+    print("="*tam)
+    print(f"  {txt}")
+    print("="*tam)
+
 while True:
+    titulo("HELP SYSTEM - PyHELP")
     comando = str(input("Função ou biblioteca: "))
     if comando.upper == "FIM":
         break
