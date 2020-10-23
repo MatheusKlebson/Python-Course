fb93a66cd613038b140e8398fe964687fe04f4ff
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-23 20:20:20 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Função para fatorial - 102.py
+++ TERCEIRO MUNDO - THIRD WORLD/Função para fatorial - 102.py
@@ -1,29 +1,14 @@
 #  Exercício Python 102: Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
 #  o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional)
 #  indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
-def factorial(n,show):
-    print("="*50)
+def factorial(calculation,show):
     f = 1
-    for cont in range(n,0,-1):
+    for counter in (calculation,0,-1):
         if show == True:
-            print(f"{cont}",end="")
-            if cont > 1:
+            print(f"{counter}",end="")
+            if counter > 1:
                 print(" X ",end="")
             else:
                 print(" = ",end="")
-        f *= cont
-    return f
 
 
-print("="*50)
-num = int(input("Digite um número para o fatorial: "))
-show = " "
-while show not in "SN":
-    show = str(input("MOSTRA FATORIAL[S/N]: ")).upper().strip()[0]
-    if show == "S":
-        show = True
-        break
-    if show == "N":
-        show = False
-        break
-print(factorial(num,show))
