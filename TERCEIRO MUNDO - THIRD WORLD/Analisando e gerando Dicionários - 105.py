37d6a299be1dc3af6c5694e57cb75452c9fd444c
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-24 18:02:07 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Analisando e gerando Dicionários - 105.py
+++ TERCEIRO MUNDO - THIRD WORLD/Analisando e gerando Dicionários - 105.py
@@ -5,8 +5,8 @@
 – A menor nota                                                                                                                                                              
 – A média da turma                                                                                                                                                      
 – A situação (opcional)'''
-def notes(*num):
-     '''
+def notes(*num,situation=False):
+    '''
     FUNCTION FOR CALCULATING MANY NUMBERS:
     def(*num,situation=False) 
     RECEIVES PARAMETERS
@@ -19,16 +19,17 @@ def notes(*num):
     dictionary["MAXIMUM_NOTE"] = max(num)
     dictionary["MINIMUM_NOTE"] = min(num)
     dictionary["CLASS_AVERAGE"] = sum(num)/len(num)
-    if dictionary["CLASS_AVERAGE"] >= 7 and dictionary["CLASS_AVERAGE"] <= 10:
-        dictionary["SITUATION"] = "Very Good"
-    elif dictionary["CLASS_AVERAGE"] >= 5 and dictionary["CLASS_AVERAGE"] < 7:
-        dictionary["SITUATIOM"] = "Reasonable"
-    elif dictionary["CLASS_AVERAGE"] >= 0 and dictionary["CLASS_AVERAGE"] < 5:
-        dictionary["SITUATIOM"] = "Very Bad"
-    else:
-        dictionary["SITUATIOM"] = "Medium Invalid"
+    if situation == True:
+        if dictionary["CLASS_AVERAGE"] >= 7 and dictionary["CLASS_AVERAGE"] <= 10:
+            dictionary["SITUATION"] = "Very Good"
+        elif dictionary["CLASS_AVERAGE"] >= 5 and dictionary["CLASS_AVERAGE"] < 7:
+            dictionary["SITUATIOM"] = "Reasonable"
+        elif dictionary["CLASS_AVERAGE"] >= 0 and dictionary["CLASS_AVERAGE"] < 5:
+            dictionary["SITUATIOM"] = "Very Bad"
+        else:
+            dictionary["SITUATIOM"] = "Medium Invalid"
     return dictionary
 
 
-
-
+response = notes(10,10,10,10,situation=True)
+print(response)
