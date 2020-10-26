03459b1495dcdec3b081746e0992b4ddfdcf60a8
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-26 15:45:54 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex107/Programa.py
+++
@@ -1,8 +0,0 @@
-# Exercício Python 107: Crie um módulo chamado moeda.py que tenha as 
-# funções incorporadas aumentar(), diminuir(), dobro() e metade(). 
-# Faça também um programa que importe esse módulo e use algumas dessas funções.
-from Moeda import increase,decrease,double,half
-price_product = float(input("HOW MUCH THE PRODUCT COSTS?U$"))
-print(f"The double is {double(price_product)}")
-print(f"The half is {half(price_product)}")
-print(f"The increase from 10% is {increase(price_product,10)}")
--- TERCEIRO MUNDO - THIRD WORLD/ex108/Programa.py
+++
@@ -1,8 +0,0 @@
-# Exercício Python 108: Adapte o código do desafio #107, 
-# criando uma função adicional chamada moeda() que consiga mostrar os números 
-# como um valor monetário formatado.
-from ex108 import moeda
-
-price_product = float(input("HOW MUCH THE PRODUCT COSTS?U$"))
-print(f"A metade de {moeda.moeda(price_product)} é {moeda.moeda(moeda.metade(price_product))}")
-
