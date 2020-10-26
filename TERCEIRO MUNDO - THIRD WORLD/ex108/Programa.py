3f6409e663eb613550ada30a0abf7a3eb7e2c882
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-26 12:52:05 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex108/Programa.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex108/Programa.py
@@ -1,10 +1,8 @@
 # Exercício Python 108: Adapte o código do desafio #107, 
 # criando uma função adicional chamada moeda() que consiga mostrar os números 
 # como um valor monetário formatado.
+from ex108 import moeda
 
-
-from ex108 import Moeda
 price_product = float(input("HOW MUCH THE PRODUCT COSTS?U$"))
-print(f"The double is {moeda.Moeda(Moeda.double(price_product))}")
-print(f"The half is {moeda.Moeda(Moeda.half(price_product))}")
-print(f"The increase from 10% is {moeda.Moeda(Moeda.increase(price_product,10))}")
+print(f"A metade de {moeda.moeda(price_product)} é {moeda.moeda(moeda.metade(price_product))}")
+
