86d1bcb094873204b4a257add1675a116dd76380
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-25 22:51:01 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Exercise107/Moeda.py
+++ TERCEIRO MUNDO - THIRD WORLD/Exercise107/Moeda.py
@@ -2,13 +2,16 @@ def increase(price,rate):
     response = price + (price*rate/100)
     return response
 
+
 def decrease(price,rate):
+    response = price - (price*rate/100)
     return response
 
 
 def double(price):
     response = price * 2
     return response
+
 
 def half(price):
     response = price / 2
