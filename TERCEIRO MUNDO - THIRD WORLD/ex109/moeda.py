011ae2a68d086326bbb7b811b59eb92842605588
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-28 15:14:29 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex109/moeda.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex109/moeda.py
@@ -1,4 +1,4 @@
-def diminuir(preço=0,taxa=0,formato=False):
+def diminuir(preço=0,taxa=0,formato=True):
     resp = preço - (preço*taxa/100)
     if formato == False:
         return resp 
@@ -6,7 +6,7 @@ def diminuir(preço=0,taxa=0,formato=False):
         moeda(resp)
         return resp
 
-def aumentar(preço=0,taxa=0,formato=False):
+def aumentar(preço=0,taxa=0,formato=True):
     resp = preço + (preço*taxa/100)
     if formato == False:
         return resp 
@@ -38,15 +38,13 @@ def resumo(preço=0,taxamais=0,taxamenos=0):
     print("="*50)
     print("{:^50}".format("ANALISANDO O VALOR"))
     print("="*50)
-    produtos = ("PREÇO ANALISADO",
+    analise = ("PREÇO ANALISADO",
             "DOBRO",
             "METADE",
             f"AUMENTO DE {taxamais}%",
-            f"DESCONTO DE {taxamenos}"
+            f"DESCONTO DE {taxamenos}%"
             ) 
-    preços = (preço,dobro(preço),metade(preço),aumentar(preço),diminuir(preço)) 
-    for tabela in range(0, len(produtos)): #Laço de repetição que permite formar a tabela
-        print(f"{produtos[tabela]:.<40}: R${preços[tabela]:8.2f}")
-    '''print(f"A metade de {moeda.moeda(preço)} é {moeda.metade(preço,False)}")
-    print(f"O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço,True)}")
-    print(f"O aumento de 10%, fica {moeda.aumentar(preço,10,True)}")'''
+    dados = (preço,dobro(preço),metade(preço),aumentar(preço),diminuir(preço)) 
+    for tabela in range(0, len(analise)): #Laço de repetição que permite formar a tabela
+        print(f"{analise[tabela]}: R${dados[tabela]}")
+    
