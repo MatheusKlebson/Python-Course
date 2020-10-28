bf03d72b1be21df868885c4bab3c899bc6060825
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-28 15:04:25 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex109/moeda.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex109/moeda.py
@@ -38,6 +38,15 @@ def resumo(preço=0,taxamais=0,taxamenos=0):
     print("="*50)
     print("{:^50}".format("ANALISANDO O VALOR"))
     print("="*50)
+    produtos = ("PREÇO ANALISADO",
+            "DOBRO",
+            "METADE",
+            f"AUMENTO DE {taxamais}%",
+            f"DESCONTO DE {taxamenos}"
+            ) 
+    preços = (preço,dobro(preço),metade(preço),aumentar(preço),diminuir(preço)) 
+    for tabela in range(0, len(produtos)): #Laço de repetição que permite formar a tabela
+        print(f"{produtos[tabela]:.<40}: R${preços[tabela]:8.2f}")
     '''print(f"A metade de {moeda.moeda(preço)} é {moeda.metade(preço,False)}")
     print(f"O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço,True)}")
     print(f"O aumento de 10%, fica {moeda.aumentar(preço,10,True)}")'''
