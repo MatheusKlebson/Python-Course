df8b7d9a62806fe0c1c6031c7b8b6b42ecd2780c
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-28 14:37:34 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex109/teste.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex109/teste.py
@@ -1,6 +1,6 @@
 import moeda
 
 p = float(input("Preço do produto?R$"))
-print(f"A metade de {moeda.moeda(p)} é {moeda.metade(p)}")
-print(f"O dobro de {moeda.moeda(p)} é {moeda.dobro(p)}")
-print(f"O aumento de 10%, fica {moeda.aumentar(p,10)}")
+print(f"A metade de {moeda.moeda(p)} é {moeda.metade(p,True)}")
+print(f"O dobro de {moeda.moeda(p)} é {moeda.dobro(p,True)}")
+print(f"O aumento de 10%, fica {moeda.aumentar(p,10,True)}")
