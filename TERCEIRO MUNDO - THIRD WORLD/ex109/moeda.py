60740efc9cee351a6c8336f6fe2555961476e75a
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-28 14:40:31 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex109/moeda.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex109/moeda.py
@@ -1,22 +1,22 @@
-def diminuir(preço=0,taxa=0):
+def diminuir(preço=0,taxa=0,formato=False):
     resp = preço - (preço*taxa/100)
-    return resp 
+    return resp if formato == False else moeda(resp)
 
 
-def aumentar(preço=0,taxa=0):
+def aumentar(preço=0,taxa=0,formato=False):
     resp = preço + (preço*taxa/100)
-    return resp 
+    return resp if formato is False else moeda(resp)
     
 
 
-def metade(preço=0):
+def metade(preço=0,formato=False):
     resp = preço / 2
-    return resp 
+    return resp if formato is False else moeda(resp)
 
 
-def dobro(preço=0):
+def dobro(preço=0,formato=False):
     resp = preço * 2
-    return resp 
+    return resp if formato is False else moeda(resp)
     
-def moeda(preço=0,moeda="R$"):
+def moeda(preço=0,moeda="R$",formato=False):
     return f"{moeda}{preço:.2f}".replace(".",",")
