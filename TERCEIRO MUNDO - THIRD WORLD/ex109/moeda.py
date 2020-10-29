4e7b73032d0c799cf0d638f0b35d5bd57700f02f
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-29 15:16:58 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex109/moeda.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex109/moeda.py
@@ -1,18 +1,18 @@
-def diminuir(preço=0,taxa=0,formato=True):
+def diminuir(preço=0,taxa=0,formato=False):
     resp = preço - (preço*taxa/100)
     if formato == False:
         return resp 
     else:
         moeda(resp)
-        return resp
 
-def aumentar(preço=0,taxa=0,formato=True):
+
+def aumentar(preço=0,taxa=0,formato=False):
     resp = preço + (preço*taxa/100)
     if formato == False:
         return resp 
     else:
         moeda(resp)
-        return resp
+      
 
 
 def metade(preço=0,formato=False):
@@ -21,30 +21,15 @@ def metade(preço=0,formato=False):
         return resp 
     else:
         moeda(resp)
-        return resp
+        
 
 def dobro(preço=0,formato=False):
     resp = preço * 2
-    if formato == False:
+    if formato is False:
         return resp 
     else:
         moeda(resp)
-        return resp
+        
     
-def moeda(preço=0,moeda="R$",formato=False):
+def moeda(preço=0,moeda="R$"):
     return f"{moeda}{preço:.2f}".replace(".",",")
-
-def resumo(preço=0,taxamais=0,taxamenos=0):
-    print("="*50)
-    print("{:^50}".format("ANALISANDO O VALOR"))
-    print("="*50)
-    analise = ("PREÇO ANALISADO",
-            "DOBRO",
-            "METADE",
-            f"AUMENTO DE {taxamais}%",
-            f"DESCONTO DE {taxamenos}%"
-            ) 
-    dados = (preço,dobro(preço),metade(preço),aumentar(preço),diminuir(preço)) 
-    for tabela in range(0, len(analise)): #Laço de repetição que permite formar a tabela
-        print(f"{analise[tabela]}: R${dados[tabela]}")
-    
