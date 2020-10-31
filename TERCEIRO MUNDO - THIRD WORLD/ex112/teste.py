ff260a27edb6a59d14776aa4bf09cce8ca6d1e68
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-31 12:51:00 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex112/teste.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex112/teste.py
@@ -1,4 +1,4 @@
 import utilidadescev
 
-p = float(input("Preço do produto?R$"))
-utilidadescev.moeda.resumo(p,10,5)
+utilidadescev.dado.leiaDinheiro("Preço do produto?R$")
+#utilidadescev.moeda.resumo(p,10,5)
