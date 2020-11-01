9f8431bf478ba00a11ff305225199920922fbaf2
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-11-01 19:40:47 -0200

    Códigos Python

---
+++ TERCEIRO MUNDO - THIRD WORLD/ex113/Funções aprofundadas.py
@@ -0,0 +1,9 @@
+'''Exercício Python 113: Reescreva a função leiaInt() que fizemos no desafio 104, 
+incluindo agora a possibilidade da digitação de um número de tipo inválido.
+Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.'''
+import validação
+
+num_int = validação.leiaInt("Digite um número inteiro: ")
+#num_real = leiaFloat("Digite um número real: ")
+print(f"O número Inteiro: {num_int}")
+#print(f"O número Real: {num_real}")
--- TERCEIRO MUNDO - THIRD WORLD/ex113/validação.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex113/validação.py
@@ -1,6 +1,3 @@
-'''Exercício Python 113: Reescreva a função leiaInt() que fizemos no desafio 104, 
-incluindo agora a possibilidade da digitação de um número de tipo inválido.
-Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.'''
 def leiaInt(mensagem):
     while True:
         try:
@@ -14,6 +11,3 @@ def leiaInt(mensagem):
         else:
             return num
             break
-
-num_int = leiaInt("Digite um número inteiro: ")
-print(f"O número inteiro: {num_int}")
