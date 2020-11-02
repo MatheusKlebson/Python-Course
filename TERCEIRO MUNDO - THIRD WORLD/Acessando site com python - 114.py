1072c8deb36ac0fac5865b807029f2fcdc87cece
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-11-02 16:34:37 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Acessando site com python - 114.py
+++ TERCEIRO MUNDO - THIRD WORLD/Acessando site com python - 114.py
@@ -3,8 +3,4 @@ o site pudim está acessível pelo computador usado.'''
 import urllib
 import urllib.request
 try:
-    site = urllib.request.urlopen("http://www.google.com.br")
-except urllib.error.URLError:
-    print("A internet não está disponível")
-else:
-    print("A internet tá funcionando tranquilamente")
+    acesso = urllib.request.urlopen("http://www.google.com.br")
