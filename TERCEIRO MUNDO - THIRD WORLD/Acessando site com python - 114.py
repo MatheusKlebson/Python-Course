9ab17ca6ae2c4bca65904d1348cf9eb3c2abd8f3
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-11-02 16:36:14 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/Acessando site com python - 114.py
+++ TERCEIRO MUNDO - THIRD WORLD/Acessando site com python - 114.py
@@ -4,3 +4,7 @@ import urllib
 import urllib.request
 try:
     acesso = urllib.request.urlopen("http://www.google.com.br")
+except urllib.error.URLError:
+    print("The web is not available")
+else:
+    
