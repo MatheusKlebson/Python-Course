f76270a1de29930dd52f66ddb777f7cf417183e3
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-11-04 19:27:22 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex115/sistema.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex115/sistema.py
@@ -1,14 +1,14 @@
 from lib.interface import *
-
+from time import sleep
 while True:
     resposta = menu(["VER PESSOAS CADASTRADAS","CADASTRAR PESSOAS","SAIR DO SISTEMA"])
     if resposta == 1:
-        print(linha())
-        print("Vendo pessoas")
-        print(linha())
+        cabeçalho("Vendo pessoas")
     elif resposta == 2:
-
+        cabeçalho("Cadastrando")
     elif resposta == 3:
-
+        cabeçalho("Saindo do sistema")
+        break
     else:
-        
+        cabeçalho("ERRO. DIGITE UMA OPÇÃO VÁLIDA")
+    sleep(2)
