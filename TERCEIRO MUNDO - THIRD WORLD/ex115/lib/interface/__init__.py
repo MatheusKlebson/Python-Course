ec75643acd200005eb9552553c0ac29257d1731d
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-11-04 19:19:30 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex115/lib/interface/__init__.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex115/lib/interface/__init__.py
@@ -13,3 +13,19 @@ def menu(lista):
     for i,opções in enumerate(lista):
         print(f"{i + 1} - {opções}")
     print(linha())
+    opc = leiaInt("Sua opção: ")
+    return opc
+
+
+def leiaInt(mensagem):
+    while True:
+        try:
+            num = int(input(mensagem))
+        except (ValueError,TypeError):
+            print("ERRO!! Por favor digite um valor inteiro...")
+        except (KeyboardInterrupt):
+            print("O usuário interropeu o programa, o valor será nulo.")
+            return 0
+        else:
+            return num
+            break
