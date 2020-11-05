a28a7101545a19e066b5a4d73323aaf9dae52972
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-11-05 15:55:03 -0200

    Códigos Python

--- TERCEIRO MUNDO - THIRD WORLD/ex115/lib/arquivo/__int__.py
+++ TERCEIRO MUNDO - THIRD WORLD/ex115/lib/arquivo/__int__.py
@@ -2,6 +2,7 @@ def arquivoExiste(nome):
     try:
         a = open(nome, "rt")
         a.close()
-        return True
-    except FileExistsError:
+    except FileNotFoundError:
         return False
+    else:
+        return True
